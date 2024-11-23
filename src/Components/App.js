0d3c7355acf6f66dfb90a13df89d891e8e4724a9
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-11-23 22:20:20 +0100

    23-11-22

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import PackingList from "./PackingList";
 import Logo from "./Logo";
 import Form from "./Form";
 import Stats from "./Stats";
+import "./index.css"
 
 export default function App() {
   const [items, setItems] = useState([]);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import "./index.css";
+// import "./index.css";
 import ReactDOM from "react-dom/client";
-// import Challenge from "./Challenge";
-import App from "./Components/App";
-// import FinalChallenge from "./FinalChallenge";
+// import Challenge from "./Challenge/Challenge";
+// import App from "./Components/App";
+import Accordion from "./Exercise/Accordion";
+// import FinalChallenge from "./Challenge/FinalChallenge";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Accordion />
     {/* <Challenge /> */}
     {/* <FinalChallenge /> */}
   </React.StrictMode>
