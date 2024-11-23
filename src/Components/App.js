2e5f537d6de6221b8bf917dbbae5ddd50ddfe16b
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2024-11-23 16:09:49 +0100

    23-11-16

---
+++ src/Components/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import PackingList from "./PackingList";
+import Logo from "./Logo";
+import Form from "./Form";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        setItems={setItems}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./index.css";
 import ReactDOM from "react-dom/client";
-import Challenge from "./Challenge";
-import App from "./App";
-import FinalChallenge from "./FinalChallenge";
+// import Challenge from "./Challenge";
+import App from "./Components/App";
+// import FinalChallenge from "./FinalChallenge";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
